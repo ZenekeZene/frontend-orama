ad5c3512414f983a810fee1d837105bc15aa18f4
ZenekeZene <hectorvillarm@gmail.com>
2020-02-20 18:50:41 +0100

    Result image

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -2,7 +2,7 @@
   <article class="result" page>
     <span class="icon-home" @click="goToStart"></span>
     <h1>Resultado: {{ points }} / {{ totalQuestions }}</h1>
-    <img src="../assets/scan_QR.svg" />
+    <img src="../assets/scan_QR.svg" class="result__image" />
   </article>
 </template>
 <script>
